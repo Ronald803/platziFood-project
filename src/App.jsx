0b28bffd2f1e735ac857d555552f059b53621f25
Ronald803 <student13@jala-foundation.org>
2024-01-09 21:12:37 -0400

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <body className='min-h-screen bg-gray-400'>
+      <div>
+        <div>
+          <img src="" alt=""/>
+        </div>
+        <h2>titulo</h2>
+        <p>descripcion</p>
+      </div>      
+    </body>
+  )
+}
+
+export default App
